0cc20e07a80d36e86c5b5828a27c3717638e854d
Vasiliy <v.nichnikov@gmail.com>
2020-05-17 13:49:42 +0300

    Первая загрузка игры

---
+++ Jumper/Assets/Scripts/Game/FollowingCamera.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Данный скрипт следует за игроком
+public class FollowingCamera : MonoBehaviour
+{
+    [Header("Объект за которым следит камера")] [SerializeField]
+    private Transform _transformTarget = null;
+    
+    [Header("Скрипт, который управлляет джампером")] [SerializeField]
+    private ManagingJumper _managingJumper = null;
+    
+    [Header("Скорость движения камеры")] [SerializeField]
+    private float _speedCamera = .0f;
+
+    [Header("Разница расстояния от камеры до объекта")] [SerializeField]
+    private float DifferenceDistances = .0f;
+    
+    
+    // Позиция камеры
+    private Transform _thisTransform = null;
+
+    private void Start()
+    {
+        _thisTransform = transform;
+    }
+
+    private void Update()
+    {
+        //float distance = Vector3.Distance(new Vector3(_thisTransform.position.x, 0, _thisTransform.position.z), 
+        //    new Vector3(_transformTarget.position.x, 0, _transformTarget.position.z));
+        //print(distance);
+        
+        Vector3 targetDir = _transformTarget.position - transform.position;
+        targetDir = new Vector3(targetDir.x, 0, targetDir.z);
+        float angle = Vector3.Angle(targetDir, transform.forward);
+        // Получаем угол от середины камеры до объекта
+        //print(angle);
+        //Quaternion.Angle(Quaternion.Euler(targetDir), Quaternion.Euler(transform.forward))
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.black);
+        Debug.DrawRay(transform.position, targetDir * 1000, Color.red);
+    }
+
+    // (Доделать поворот угла)
+    private void LateUpdate()
+    {
+        // Позиция, до которой нужно двигаться 
+        float positionEndX = _transformTarget.transform.position.x - DifferenceDistances;
+        _thisTransform.localPosition = new Vector3(Mathf.MoveTowards(_thisTransform.localPosition.x, positionEndX, 
+            _managingJumper.GetSpeedUpJumper * Time.deltaTime), _thisTransform.localPosition.y, _thisTransform.localPosition.z); 
+        
+        Vector3  differenceVector = _transformTarget.position - transform.position;
+        differenceVector = new Vector3(differenceVector.x, 0, differenceVector.z);
+        // Вычисляем разницу между камерой и джампером
+        float angle = Vector3.Angle(differenceVector, transform.forward);
+        //print(angleCameraY - angle);
+        //print(_transformTarget.rotation.y + ":" + Quaternion.);
+        //_thisTransform.localRotation = Quaternion.Slerp(_thisTransform.rotation, 
+        //    Quaternion.Euler(0,  GetLessAngle() - angle, 0), 2 * Time.deltaTime);
+        //_thisTransform.LookAt(_transformTarget);
+    }
+
+    // Возвращает актуальный угол, от 0 до 180 (Доделать)
+    private float GetLessAngle()
+    {
+        float angleCameraY = _thisTransform.transform.eulerAngles.y;
+        // if (angleCameraY > 180)
+        // {
+        //     //print(angleCameraY - 360);
+        //     return angleCameraY - 360;
+        //}
+
+        return angleCameraY;
+    }
+    
+}
---
+++ Jumper/Assets/Scripts/Game/InspectionGround.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Данный класс проверяет место на какой поверхности находится джампер
+public class InspectionGround : MonoBehaviour
+{
+    [Header("Скрипт, управляет джампером")] [SerializeField]
+    private ManagingJumper _managingJumper = null;
+    
+    // Проверяет, находится ли джампер на земле или нет
+    public static bool IsGround = false;
+    
+    // Очистка кэша
+    private Transform _thisTransform = null;
+
+    private void Start()
+    {
+        _thisTransform = transform;
+    }
+    
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.tag == "ground")
+        {
+            print("Ground:" + other.gameObject.name);
+        }
+    }
+    
+    private void OnCollisionStay(Collision other)
+    {
+        //if (other.collider.tag == "ground")
+        //{
+        print(other.gameObject.name + " Enter"); 
+        _managingJumper.StartAnimationTopJumper();
+        _managingJumper.ChangeRigidbodyKinematic(true);
+        IsGround = true;
+        //}
+    }
+
+    // private void OnCollisionExit(Collision other)
+    // {
+    //     print(other.gameObject.name + " Exit");
+    //     IsGround = false;
+    // }
+
+    private void Update()
+    {
+        // RaycastHit hit;
+        // float distanceHit = 10.0f;
+        // Debug.DrawRay(_thisTransform.position, transform.TransformDirection(Vector3.down), Color.yellow, distanceHit);
+        // Ray ray = new Ray(_thisTransform.position, transform.TransformDirection(Vector3.down * 0.01f));
+        // Physics.Raycast(ray, out hit, distanceHit);
+        //
+        // //print(hit.collider.gameObject.name);
+        // if (hit.collider != null && hit.collider.tag == "ground")
+        // {
+        //     Vector3 objectPosition = hit.collider.transform.position;
+        //     print(Vector3.Distance(_thisTransform.position, objectPosition) + " " + hit.collider.gameObject.name);
+        //     float distance = Vector3.Distance(_thisTransform.position, objectPosition);
+        //     if (distance <= 1)
+        //     {
+        //         //print("Ground");
+        //         _managingJumper.ChangeRigidbodyKinematic(true);
+        //         IsGround = true;
+        //     }
+        // }
+    }
+}
