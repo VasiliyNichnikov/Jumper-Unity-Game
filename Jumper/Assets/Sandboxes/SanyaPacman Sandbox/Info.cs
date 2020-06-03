6f16880bd34c4a188476fb6de8a87f32513bb083
SanyaPacman <SanyaPacman@yandex.ru>
2020-06-03 14:52:27 +0300

    кнопка в инспекторе. не работает

---
+++ Jumper/Assets/Sandboxes/SanyaPacman Sandbox/Info.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(OpenFileButtonScript))]
+public class Info : Editor
+{
+    public Vector3 Point1;
+    public Vector3 Point2;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        OpenFileButtonScript myScript = (OpenFileButtonScript)target;
+        if (GUILayout.Button("Open File"))
+        {
+            myScript.OpenDialog();
+        }
+    }
+
+}
