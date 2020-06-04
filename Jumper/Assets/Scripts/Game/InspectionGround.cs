2585f34887e056d488dcef5df937f31e2fb06b67
Vasiliy <v.nichnikov@gmail.com>
2020-06-04 21:56:25 +0300

    Работа над джампром
    Добавлен прыжок и смена высоты верхней части. В дальнейшем надо работать над физикой, а также над приземлением.

--- Jumper/Assets/Scripts/Game/InspectionGround.cs
+++ Jumper/Assets/Scripts/Game/InspectionGround.cs
@@ -6,59 +6,38 @@ using UnityEngine;
 // Данный класс проверяет место на какой поверхности находится джампер
 public class InspectionGround : MonoBehaviour
 {
-    [Header("Скрипт, управляет джампером")] [SerializeField]
-    private ManagingJumper _managingJumper = null;
+    // Rigidbody игрока
+    private Rigidbody _rigidbodyJumper = null;
     
-    // Проверяет, находится ли джампер на земле или нет
-    public static bool IsGround = false;
     
-    // Очистка кэша
-    //private Transform _thisTransform = null;
 
     private void Start()
     {
-        //_thisTransform = transform;
+        _rigidbodyJumper = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    public IEnumerator CheckGround()
     {
-        //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 10, Color.black);
-        RaycastHit hit;
-        
-        if (Physics.SphereCast(transform.position, 0.18f / 2, transform.TransformDirection(Vector3.down), out hit, 0.5f))
+        while (true)
         {
-            print(hit.collider.name);
-        }
-        
-        // Debug.DrawRay(Physics.SphereCast(transform.position, 2,  transform.TransformDirection(Vector3.forward) * 10, out hit));
-    }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.collider.tag == "ground")
-        {
-            //print("Ground:" + other.gameObject.name);
+            RaycastHit hit;
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * 10, Color.black); 
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 0.09f)) 
+            {
+                
+                if (hit.collider.tag == "ground" || hit.collider.tag == "Block")
+                {
+                    _rigidbodyJumper.isKinematic = true;
+                    yield break;
+                }
+            }
+            yield return null;
         }
     }
-    
-    private void OnCollisionStay(Collision other)
-    {
-        //if (other.collider.tag == "ground")
-        //{
-        // Vector3 targetDir = transform.position - other.transform.position;
-        // float angle = Vector3.Angle(targetDir, Vector3.right);
-        // print(angle);
-        
-        //rint(other.gameObject.name + " Enter"); 
-        _managingJumper.StartAnimationTopJumper();
-        _managingJumper.ChangeRigidbodyKinematic(true);
-        IsGround = true;
-        print(IsGround);
-        //}
-    }
 
-    public float GetHeight()
+    private void OnDrawGizmosSelected()
     {
-        return transform.position.y;
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(transform.position + transform.TransformDirection(Vector3.down) * 0.05f, 0.06242963f);
     }
 }
--- Jumper/Assets/Scripts/Game/ManagingJumper.cs
+++ Jumper/Assets/Scripts/Game/ManagingJumper.cs
@@ -8,195 +8,267 @@ public class ManagingJumper : MonoBehaviour
     [Header("Главная камера")] [SerializeField]
     private Camera _mainCamera = null;
 
-    [Header("Основная часть джампера")] [SerializeField]
-    private GameObject _mainJumper = null;
-    
-    [Header("Верхняя часть джемпера")] [SerializeField]
-    private Transform _upperPartJumper = null;
+    [Header("Джампер (Игрок)")] [SerializeField]
+    private GameObject _jumper = null;
 
-    [Header("RigidBody джампера")] [SerializeField]
-    private Rigidbody _rigidbodyJumper = null;
+    [Header("Скрипт, который проверяет, находится ли игрок на земле или нет")] [SerializeField]
+    private InspectionGround _inspectionGround = null;
     
-    //[Header("Скрипт, который управляет камерой")] [SerializeField]
-   // private FollowingCamera _followingCamera = null;
     
-    [Header("Максимальный угол наклона по модулю")] [SerializeField]
-    private float _maximumAngleInclination = .0f;
+    //
+    // [Header("Основная часть джампера")] [SerializeField]
+    // private GameObject _mainJumper = null;
+    //
+    // [Header("Верхняя часть джемпера")] [SerializeField]
+    // private Transform _upperPartJumper = null;
+    //
+    // [Header("RigidBody джампера")] [SerializeField]
+    // private Rigidbody _rigidbodyJumper = null;
+    //
+    // [Header("Максимальный угол наклона по модулю")] [SerializeField]
+    // private float _maximumAngleInclination = .0f;
+    //
+    // [Header("Максимальная скорость прыжка")] [SerializeField] [Range(0, 100)]
+    // private float _maximumJumpSpeed = .0f;
+    //
+    // [Header("Минимальное сжатие джампа")] [SerializeField] [Range(0f, 1f)]
+    // private float _minimumJumpCompression = .0f;
+    //     
+    // [Header("Максимальное сжатие джампа")] [SerializeField] [Range(0f, 1f)]
+    // private float _maximumJumpCompression = .0f;
+    //
+    // // Разница координат по X (Нужно для понимания, куда идет палец (Право/Влево))
+    // private float _differenceX = .0f;
+    //
+    // //"Скорость поднятия на высоту джампера"
+    // private float _speedUpJumper = .0f;
+    //
+    //
+    // // Данная переменная, проверяет анимируется ли верхняя часть джампера или нет
+    // private bool _animationTopPartJumper = false;
+    
+    // Переменные, которые отвечают за угол игрока
+    [Header("Максимальный угол, на который может повернуться джампер по оси Z")] [SerializeField] [Range(0, 90)]
+    private float _maximumJumperAngleZ = 0.0f;
 
+    // Переменный, которые отвечают за высоту второй части джампера (Верхней части)
+    [Header("Верхняя часть джампера")] [SerializeField]
+    private Transform _jumperPartUpper = null;
+    
+    [Header("Минимальное сжатие джампера")] [SerializeField] [Range(0f, 1f)]
+    private float _minimumJumpCompression = .0f;
+    
+    [Header("Максимальное сжатие джампера")] [SerializeField] [Range(0f, 1f)] 
+    private float _maximumJumpCompression = .0f;
+    
     [Header("Максимальная скорость прыжка")] [SerializeField] [Range(0, 100)]
     private float _maximumJumpSpeed = .0f;
     
-    [Header("Минимальное сжатие джампа")] [SerializeField] [Range(0f, 1f)]
-    private float _minimumJumpCompression = .0f;
-        
-    [Header("Максимальное сжатие джампа")] [SerializeField] [Range(0f, 1f)]
-    private float _maximumJumpCompression = .0f;
-    
-    // Разница координат по X (Нужно для понимания, куда идет палец (Право/Влево))
-    private float _differenceX = .0f;
-
     //"Скорость поднятия на высоту джампера"
     private float _speedUpJumper = .0f;
     
-    // Возвращает скорость джампера
-    // public float GetSpeedUpJumper
-    // {
-    //     get { return _speedUpJumper; }
-    // }
-    //
-    // public float GetDifferenceX
-    // {
-    //     get { return _differenceX; }
-    // }
-    
-    // Стартовые позиции
-    private Vector2 _startingPosition = Vector2.zero;
+    //[Header("Высота, которая должна быть в данный момент у верхней части")] [SerializeField] [Range(0, 10)] // Test
+    //private float _jumperHeightY = 0.0f;
 
-    // Данная переменная, проверяет анимируется ли верхняя часть джампера или нет
-    private bool _animationTopPartJumper = false;
+    [Header("Вектор начала")] [SerializeField]
+    private Transform _vectorStart = null;
     
-    public void ChangeRigidbodyKinematic(bool condition)
+    [Header("Вектор кинца")] [SerializeField]
+    private Transform _vectorEnd = null;
+    
+    // Дополнительные переменные
+    // Стартовая позиция мыши (пальца)
+     private Vector2 _startFingerPosition = Vector2.zero;
+
+     private Rigidbody _rigidbodyJumper = null;
+     
+    private void Start()
     {
-        _rigidbodyJumper.isKinematic = condition;
+        //_jumperHeightY = _jumperPartUpper.localPosition.y;
+        _rigidbodyJumper = _jumper.GetComponent<Rigidbody>();
     }
+
+    private void Update()
+    {
+        //_jumperPartUpper.localPosition = new Vector3(_jumperPartUpper.localPosition.x, _jumperHeightY, _jumperPartUpper.localPosition.z);
+        //_jumper.transform.localRotation = Quaternion.Euler(_jumper.transform.localRotation.x, _jumper.transform.localRotation.y, -_jumperAngleZ);
+    }
+    
+    // Рисование кнопки для запуска джампера (Создан для тестирования)
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(20, 20, 150, 100), "Start"))
+        {
+            Rigidbody _rigidbodyJumper = _jumper.GetComponent<Rigidbody>();
+            _rigidbodyJumper.isKinematic = false;
+            _rigidbodyJumper.AddForce((_vectorEnd.position - _vectorStart.position) * 12);
+            StartCoroutine(ReturnStartingTopJumperPosition(2));
+            StartCoroutine(ReturnStartingTopJumperRotation());
+            StartCoroutine(StartCheckGround());
+        }
+        
+    }
+
+    private IEnumerator StartCheckGround()
+    {
+        Vector3 startPositionJumper = _jumper.transform.position;
+        while (true)
+        {
+            if (_jumper.transform.position.y > startPositionJumper.y + 0.1f)
+            {
+                yield return _inspectionGround.CheckGround();
+                yield break;
+            }
+            yield return null;
+        }
+    }
+    
+    
+    // public void ChangeRigidbodyKinematic(bool condition)
+    // { 
+    //     _jumper.GetComponent<Rigidbody>().isKinematic = condition;
+    // }
 
     public void StartAnimationTopJumper()
     {
-        if(_upperPartJumper.localPosition.y != 1 && !_animationTopPartJumper)
-            StartCoroutine(ReturnStartingTopJumperPosition(1, 1));
+        //if(_upperPartJumper.localPosition.y != 1 && !_animationTopPartJumper)
+        //    StartCoroutine(ReturnStartingTopJumperPosition(1, 1));
     }
     
 
     private void OnMouseDown()
     {
-        if (InspectionGround.IsGround)
-        {
-            _startingPosition = new Vector2(_mainCamera.ScreenToViewportPoint(Input.mousePosition).x, 
+        //if (InspectionGround.IsGround)
+        //{
+            _startFingerPosition = new Vector2(_mainCamera.ScreenToViewportPoint(Input.mousePosition).x, 
             _mainCamera.ScreenToViewportPoint(Input.mousePosition).y);
-        }
+        //}
     }
 
     private void OnMouseDrag()
     {
-        print("Drag");
-        if (InspectionGround.IsGround)
-        {
-            print("IsGround");
-            _upperPartJumper.localPosition = new Vector3(
-                _upperPartJumper.localPosition.x, 
-                GetPositionJumper(), 
-                _upperPartJumper.localPosition.z);
-
-            _rigidbodyJumper.gameObject.transform.localRotation = Quaternion.Euler(
-                _upperPartJumper.localRotation.x,
-                _upperPartJumper.localRotation.y, 
-                GetAngleRotationJumper());
-        }
+        // if (InspectionGround.IsGround)
+        // {
+        _jumperPartUpper.localPosition = new Vector3(_jumperPartUpper.localPosition.x, 
+            GetPositionJumper(), _jumperPartUpper.localPosition.z);
+        
+        _jumper.transform.localRotation = Quaternion.Euler(_jumper.transform.localRotation.x, _jumper.transform.localRotation.y, 
+            GetAngleJumper());
+        // }
     }
     
     private void OnMouseUp()
     {
-        if (InspectionGround.IsGround)
-        {
-            AddSpeedJumperY(GetPositionX(), GetPositionJumper());
-            if(!_animationTopPartJumper)
-                StartCoroutine(ReturnStartingTopJumperPosition(GetPositionTopPartJumper(), 1.5f));
-            StartCoroutine(ReturnStartingTopJumperRotation());
-            //StartCoroutine(_followingCamera.AnimationCamera(_differenceX));
-            InspectionGround.IsGround = false;
-        }
+        Rigidbody _rigidbodyJumper = _jumper.GetComponent<Rigidbody>();
+        // Выключаем кинематику
+        _rigidbodyJumper.isKinematic = false;
+        // Вычисляем скорость
+        var percentagePositionY = 100 - GetPositionJumper() * 100;
+        //print(percentagePositionY);
+        _speedUpJumper = percentagePositionY * _maximumJumpSpeed / 100;
+        _speedUpJumper = Mathf.Clamp(_speedUpJumper, 0, _maximumJumpSpeed);
+        // Добавляем скорость
+        //print(_speedUpJumper);
+        _rigidbodyJumper.AddForce((_vectorEnd.position - _vectorStart.position) * _speedUpJumper);
+        StartCoroutine(ReturnStartingTopJumperPosition(2));
+       // StartCoroutine(ReturnStartingTopJumperRotation());
+        StartCoroutine(StartCheckGround());
+        
+        // _rigidbodyJumper.AddForce(new Vector3(positionX, 1, 0) * _speedUpJumper);
+        
+        // if (InspectionGround.IsGround)
+        // {
+        //     AddSpeedJumperY(GetPositionX(), GetPositionJumper());
+        //     if(!_animationTopPartJumper)
+        //         StartCoroutine(ReturnStartingTopJumperPosition(GetPositionTopPartJumper(), 1.5f));
+        //     //StartCoroutine(ReturnStartingTopJumperRotation());
+        //     //StartCoroutine(_followingCamera.AnimationCamera(_differenceX));
+        //     InspectionGround.IsGround = false;
+        // }
     }
     
     // Возвращает до какой позиции надо сдвигать высшую ножку джампера
-    private float GetPositionTopPartJumper()
-    {
-        var percent = _speedUpJumper * 100 / _maximumJumpSpeed;
-        //print(percent);
-        if (percent > 30)
-            return 0.8f;
-        return 1f;
-    }
+    //private float GetPositionTopPartJumper()
+    //{
+        // var percent = _speedUpJumper * 100 / _maximumJumpSpeed;
+        // //print(percent);
+        // if (percent > 30)
+        //     return 0.8f;
+        // return 1f;
+        //return 1f;// test
+    //}
     
     // Сделать изменение в зависимости от скорости
-    private float GetPositionX()
-    {
-        float posX = 1f;
-        //print(GetAngleRotationJumper());
-        if (GetAngleRotationJumper() > 3f)
-            posX = -1f;
-        else if (GetAngleRotationJumper() <= 3f && GetAngleRotationJumper() >= 0)
-            posX = 0.0f;
-        return posX;
-    }
+    //private float GetPositionX()
+    //{
+    //    // float posX = 1f;
+        // if (GetAngleRotationJumper() > 3f)
+        //     posX = -1f;
+        // else if (GetAngleRotationJumper() <= 3f && GetAngleRotationJumper() >= 0)
+        //     posX = 0.0f;
+        // return posX;
+    //    return 1f;// test
+    //}
     
     // Возвращает сжатие прыжка
     private float GetPositionJumper()
     {
         float positionNowY = _mainCamera.ScreenToViewportPoint(Input.mousePosition).y;
-        float differenceY = _startingPosition.y - positionNowY;
+        float differenceY = _startFingerPosition.y - positionNowY;
         float percentageScreenY = 100 - differenceY * 100;
         float jumpCompressionY = percentageScreenY * _maximumJumpCompression / 100;
         jumpCompressionY = Mathf.Clamp(jumpCompressionY, _minimumJumpCompression, _maximumJumpCompression);
         return jumpCompressionY;
     }
 
-    // Возвращает нужный поворот (Доделать)
-    private float GetAngleRotationJumper()
+    // Возвращает нужный поворот
+    private float GetAngleJumper()
     {
         float positionNowX = _mainCamera.ScreenToViewportPoint(Input.mousePosition).x;
-        float differenceX = _startingPosition.x - positionNowX;
-        _differenceX = differenceX;
+        float differenceX = _startFingerPosition.x - positionNowX;
 
         float percentageScreenX = differenceX * 100;
         
-        var angleInclination = percentageScreenX * _maximumAngleInclination / 100;
-        angleInclination = Mathf.Clamp(angleInclination, -_maximumAngleInclination, _maximumAngleInclination);
+        var angleInclination = percentageScreenX * _maximumJumperAngleZ / 100;
+        angleInclination = Mathf.Clamp(angleInclination, -_maximumJumperAngleZ, _maximumJumperAngleZ);
         return angleInclination;
     }
     
-    private IEnumerator ReturnStartingTopJumperPosition(float posY, float speed)
+    // Возвращает верхнюю часть джампера в первоначальное положение
+    private IEnumerator ReturnStartingTopJumperPosition(float speed, float posY = 1)
     {
-        _animationTopPartJumper = true;
-        while (_upperPartJumper.localPosition.y != posY)
+        while (_jumperPartUpper.localPosition.y != posY)
         {
-            _upperPartJumper.localPosition = Vector3.MoveTowards(_upperPartJumper.localPosition,
-                new Vector3(_upperPartJumper.localPosition.x, posY,
-                    _upperPartJumper.localPosition.z), speed * Time.deltaTime);
-            //print("work");
+            _jumperPartUpper.localPosition = Vector3.MoveTowards(_jumperPartUpper.localPosition,
+                new Vector3(_jumperPartUpper.localPosition.x, posY, _jumperPartUpper.localPosition.z),
+                speed * Time.deltaTime);
             yield return null;
         }
-        _animationTopPartJumper = false;
     }
-
+    
+    // Возвращает угол джампера в первоначальное положение
     private IEnumerator ReturnStartingTopJumperRotation()
     {
         var timeCount = .0f;
-        while (_mainJumper.transform.rotation != new Quaternion(0, 0, 0,1f))
+        while (_jumper.transform.rotation != Quaternion.Euler(0, 0, 0))
         {
-            _mainJumper.transform.rotation = Quaternion.Slerp(_mainJumper.transform.rotation, new Quaternion(0, 0, 0,1f), timeCount);
-            // Должно умножаться в зависимости от скорости
-            timeCount += Time.deltaTime * AlignmentSpeedJumper();
-            //print(AlignmentSpeedJumper());
+            //print(_jumper.transform.rotation);
+            _jumper.transform.rotation = Quaternion.Slerp(_jumper.transform.rotation, 
+                 Quaternion.Euler(0, 0, 0), timeCount);
+            // Должно умножаться в зависимости от скорости (Доработать)
+            timeCount += Time.deltaTime * 3;
             yield return null;
         }
-
-        _rigidbodyJumper.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
-        //print("End");
     }
 
-    private float AlignmentSpeedJumper()
-    {
-        return (_maximumJumpSpeed - _speedUpJumper) / 200;
-    }
-    
     private void AddSpeedJumperY(float positionX, float positionY)
     {
         // Выключаем кинематику
-        _rigidbodyJumper.isKinematic = false;
+        //_rigidbodyJumper.isKinematic = false;
         // Вычисляем скорость
-        var percentagePositionY = 100 - positionY * 100;
-        _speedUpJumper = percentagePositionY * _maximumJumpSpeed / 100;
-        _speedUpJumper = Mathf.Clamp(_speedUpJumper, 0, _maximumJumpSpeed);
-        _rigidbodyJumper.AddForce(new Vector3(positionX, 1, 0) * _speedUpJumper);
+        // var percentagePositionY = 100 - positionY * 100;
+        // _speedUpJumper = percentagePositionY * _maximumJumpSpeed / 100;
+        // _speedUpJumper = Mathf.Clamp(_speedUpJumper, 0, _maximumJumpSpeed);
+        // _rigidbodyJumper.AddForce(new Vector3(positionX, 1, 0) * _speedUpJumper);
     }
 }
