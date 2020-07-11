5d84a939a530590c8335397ddd7e71a7000c44b5
SanyaPacman <SanyaPacman@yandex.ru>
2020-07-11 09:52:18 +0300

    доработаны коины
    Ставятся временные отрезки, и монеты в эти временные отрезки

--- Jumper/Assets/Sandboxes/SanyaPacman Sandbox/Scripts/Score.cs
+++ Jumper/Assets/Sandboxes/SanyaPacman Sandbox/Scripts/Score.cs
@@ -9,13 +9,15 @@ public class Score : MonoBehaviour
     public Text ScoreText;
     public Text TimeText;
     public Text DistanceText;
-    public int CoinReward;
-    public float TimeForObj;
-    public int CapacityForLastObjects=10;
-    public float Distance=0;
-    int CurrentCoins = 0;    
-    float CurrTime;
-    public List<GameObject> lastObjects;
+
+
+    public int CapacityForLastObjects = 10;
+    public float Distance;
+    public int[] CoinReward;
+    public float[] TimeSteps;
+    int CurrentCoins = 0;
+    float CurrTime = 0;
+    List<GameObject> lastObjects;
 
     Vector3 RayCenter;
     // Start is called before the first frame update
@@ -24,49 +26,57 @@ public class Score : MonoBehaviour
         lastObjects = new List<GameObject>();
         RefreshTime();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        if (CurrTime>0)        
-            CurrTime -= Time.deltaTime;
         if (transform.position.x>0)
             Distance = transform.position.x;
 
 
+        CurrTime += Time.deltaTime;
 
-        TimeText.text =String.Format("время на объекте {0:f1}",CurrTime);
+       
+        TimeText.text = String.Format("время на объекте {0:f1}", CurrTime);
         DistanceText.text = String.Format("Рекорд в расстоянии {0:f1}", Distance);
         RaycastHit hit;
-        
-        if (Physics.Raycast(transform.position,Vector3.down, out hit, Mathf.Infinity, 9 ))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, 9))
         {
-            RayCenter = hit.point;
-            var obj = hit.transform.gameObject;            
-            if (obj.tag!="Object")            
+            var obj = hit.transform.gameObject;
+            if (obj.tag != "Object")
                 return;
-            
+            RayCenter = hit.point;
             if (!lastObjects.Contains(obj))
             {
                 lastObjects.Add(obj);
+                AddCoins();
                 RefreshTime();
-                if (lastObjects.Count>CapacityForLastObjects)                
-                    lastObjects.RemoveAt(0);                
+                if (lastObjects.Count > CapacityForLastObjects)
+                    lastObjects.RemoveAt(0);
             }
         }
 
     }
 
     private void RefreshTime()
+    {        
+        CurrTime = 0;
+        ScoreText.text = String.Format("Коины: {0}", CurrentCoins);
+    }
+    private void AddCoins()
     {
-        if (CurrTime>0)
-            CurrentCoins += CoinReward;
-        else
-            CurrentCoins += CoinReward/2;
-
-        CurrTime = TimeForObj;        
-        ScoreText.text = String.Format("Счет: {0}", CurrentCoins);
-        Debug.Log(CurrentCoins);
+        for (int i = 0; i < TimeSteps.Length - 1; i++)
+        {
+            Debug.Log(String.Format("время {0} {1} {2}", TimeSteps[i], CurrTime, TimeSteps[i + 1]));
+            if (CurrTime > TimeSteps[i] && CurrTime < TimeSteps[i + 1])
+            {
+                CurrentCoins += CoinReward[i];
+                Debug.Log(String.Format("<color=red> заработано {0} коинов</color>", CoinReward[i]));
+                return;
+            }
+        }
+        CurrentCoins += CoinReward[CoinReward.Length - 1];
+        Debug.Log(String.Format("<color=green> заработано {0} коинов</color>", CoinReward[CoinReward.Length - 1]));
     }
     private void OnDrawGizmos()
     {
